3f36dec74d7c4ddc46c83b10c9ab4735ef865a39
Frozen String <964413011@qq.com>
2022-04-10 22:23:45 +0800

    :tada: depend SubModel

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use syn::{parse_macro_input, DeriveInput};
+
+mod darling_models;
+
+#[proc_macro_derive(SubModel, attributes(sub_model))]
+pub fn sub_model_derive_marco(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _derive_input = parse_macro_input!(input as DeriveInput);
+
+
+
+    quote::quote! {}.into()
+}
